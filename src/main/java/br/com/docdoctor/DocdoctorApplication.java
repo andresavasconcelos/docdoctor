70505ae4d74d91d55bc0dba2da6b1f4bd9ba9821
andresavasconcelos <andresa.vasconcelos-30@hotmail.com>
2025-05-11 13:36:00 -0300

    main - first commit

---
+++ src/main/java/br/com/docdoctor/DocdoctorApplication.java
@@ -0,0 +1,13 @@
+package br.com.docdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocdoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocdoctorApplication.class, args);
+	}
+
+}
