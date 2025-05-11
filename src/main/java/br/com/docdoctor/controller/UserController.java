3ece406c76538f393784879a0aa0a9e0d0fe6646
andresavasconcelos <andresa.vasconcelos-30@hotmail.com>
2025-05-11 19:54:55 -0300

    main - add initial implementation user api

--- src/main/java/br/com/docdoctor/controller/UserController.java
+++ src/main/java/br/com/docdoctor/controller/UserController.java
@@ -12,13 +12,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
